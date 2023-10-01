29bcfc804384332c2db9f22c0c77297c0094b018
近藤元気 <genki7onepiece@gmail.com>
2023-10-01 16:43:48 +0900

    ファーストコミット

---
+++ Hike_App_P1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hike_App_P1
+//
+//  Created by 近藤元気 on 2023/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hike_App_P1/Hike_App_P1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_App_P1App.swift
+//  Hike_App_P1
+//
+//  Created by 近藤元気 on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct Hike_App_P1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
